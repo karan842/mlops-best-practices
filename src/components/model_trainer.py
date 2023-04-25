1106327901f0292578b1507a5a089eb9121aab55
karan842 <karanshingde@gmail.com>
2023-04-25 16:17:21 +0530

    setting experiments using MLFlow

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 from dataclasses import dataclass
 from collections import Counter
 from sklearn.model_selection import train_test_split
@@ -8,13 +9,15 @@ from sklearn.ensemble import (
     AdaBoostClassifier,
     RandomForestClassifier,
 )
-from sklearn.neighbors import KNeighborsClassifier
 from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
 from sklearn.linear_model import LogisticRegression
 
 from sklearn.metrics import (
-     f1_score, accuracy_score, roc_auc_score
+     f1_score, accuracy_score, roc_auc_score, precision_score, recall_score
 )
+import mlflow
+from mlflow.tracking import MlflowClient
 
 from src.exception import CustomException
 from src.logger import logging
@@ -43,13 +46,18 @@ class ModelTrainer:
            
             models = {
                 "Random Forest": RandomForestClassifier(),
-                "Xgboost": XGBClassifier(),
-                "Adaboost": AdaBoostClassifier(),
-                "KNN": KNeighborsClassifier(),
+                "XGBoost": XGBClassifier(),
+                "LGBM": LGBMClassifier(),
+                "AdaBoost": AdaBoostClassifier(),
                 "Logistic Regression": LogisticRegression(),
             }
             
-            model_report:dict = evaluate_models(X_train,y_train,X_valid,y_valid,models)
+            # loading parameters for tuning 
+            with open('artifacts/params.json') as params_file:
+                params = json.load(params_file)
+            
+            model_report:dict = evaluate_models(X_train,y_train,X_valid,y_valid,
+                                                models,params)
             
             best_model_score = max(sorted(model_report.values()))
             
@@ -67,12 +75,45 @@ class ModelTrainer:
                 obj = best_model
             )
             
-            predicted_test = best_model.predict(X_test)
+            # log best model and its parameters with MLflow 
+            # with mlflow.start_run(run_name="Best Model"):
+            #     # log hyperparameters
+            #     for key, value in best_model.get_params().items():
+            #         mlflow.log_param(key,value)
+                    
+            #     # train and log model artifact
+            #     if isinstance(best_model,XGBClassifier):
+            #         best_model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)])
+            #         mlflow.xgboost.log_model(best_model,"model")
+            #     elif isinstance(best_model, CatBoostClassifier):
+            #         best_model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)],verbose=False)
+            #         mlflow.catboost.log_model(best_model,"model")
+            #     else:
+            #         best_model.fit(X_train, y_train)
+            #         mlflow.sklearn.log_model(best_model,"model")
             
+            # Log metrics
+            predicted_test = best_model.predict(X_test)
+            accuracy_score_test = accuracy_score(y_test, predicted_test)
+            precision_score_test = precision_score(y_test, predicted_test)
+            recall_score_test = recall_score(y_test, predicted_test)
+            f1_score_test = f1_score(y_test, predicted_test)
             roc_auc_score_test = roc_auc_score(y_test, predicted_test)
             
+            metrics = {'Best Model':best_model_name,
+                       'Accuracy':accuracy_score_test,
+                       'Precision':precision_score_test,
+                       'Recall':recall_score_test,
+                       'ROC-AUC':roc_auc_score_test}
+            
+            # for key, value in metrics.items()
+            
+            with open('artifacts/metrics.json','w') as metrics_file:
+                json.dump(metrics,metrics_file,indent=4)
+            logging.info("Metrics saved")
+            
             return(
-                roc_auc_score_test
+                metrics
             )
             
         except Exception as e:
